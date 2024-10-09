e575dc4d3d95a2faf1160de267be800179337d33
ksaito1844 <ksaito1844@gmail.com>
2024-10-09 14:38:44 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -33,16 +33,17 @@ export default function App() {
 
   return (
     <main>
-      <h1>Globalie - English tuturing platform (under construction)</h1>
+      <h1>Globalie - English tuturing platform</h1>
       <br />How it works:
+      <br />
       <br />1. Login
-      <br />2. Matching between student and teacher
-      <br />3. English learning via one-on-one audio call
-      <br />4. Audio is converted to text for ease of learning
-      <br />5. AI-driven guidance for teaching materials
-      <br />6. Text is monitored for inappropreat contents for safe learning environment
+      <br />2. Matching a student with a teacher, manualy or randomly
+      <br />3. English learning via one-on-one audio call (Amazon Chime)
+      <br />4. Audio conversation is converted to text for ease of learning (speech-to-text program)
+      <br />5. AI-driven guidance to lead teacheres (TBD)
+      <br />6. Text is monitored for inappropreate contents for a safe learning environment (content monitoring program)
 
-      <button onClick={createTodo}>Please share your idea for improvement!</button>
+      <button onClick={createTodo}>Start-up business milestones</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
