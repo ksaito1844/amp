6e96cd26abebb33423524373ef1a16d886844ac6
ksaito1844 <ksaito1844@gmail.com>
2024-10-09 15:09:39 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -44,6 +44,7 @@ export default function App() {
       <br />6. Text is monitored for inappropreate contents for a safe learning environment (content monitoring program)
       <br />
       <br />Social impacts:
+      <br />
       <br />1. Low-cost English learning platform -> More frequent learning opportunities
       <br />2. English learning from situation-based "chats" -> Practical communication for specific situations (e.g., business, school, private)
       <br />3. AI-driven teaching guidance -> Anyone with English fluency can teach, rejoining labor force towards a better society!
